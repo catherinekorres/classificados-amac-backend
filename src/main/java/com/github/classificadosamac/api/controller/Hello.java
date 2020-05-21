2d3be8d1286868b972ef88040d32d32cb41e3663
Catherine Korres <cathy.korres@gmail.com>
2020-05-20 22:37:08 -0300

    Editando resposta para um array de produtos

--- src/main/java/com/github/classificadosamac/api/controller/Hello.java
+++ src/main/java/com/github/classificadosamac/api/controller/Hello.java
@@ -2,21 +2,45 @@ package com.github.classificadosamac.api.controller;
 
 import com.github.classificadosamac.api.model.Produto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/hello")
-public class Hello {
-    @GetMapping
-    public ResponseEntity<Produto> hello() {
-        Produto produto = new Produto();
-        produto.setId(1L);
-        produto.setNome("Bolo de maçã");
-        produto.setPreco(30.0);
-        produto.setDescricao("Sem glúten e sem leite");
+import java.util.ArrayList;
+import java.util.List;
 
-        return ResponseEntity.ok(produto);
+@RestController
+@RequestMapping("/produtos")
+public class Hello {
+
+    @CrossOrigin(origins = "*")
+    @GetMapping
+    public ResponseEntity<List<Produto>> getProdutos() {
+        List<Produto> produtoList = new ArrayList<>();
+        Produto produto1 = new Produto();
+        Produto produto2 = new Produto();
+        Produto produto3 = new Produto();
+
+        produto1.setId(1L);
+        produto1.setNome("Bolo de maçã");
+        produto1.setPreco(30.0);
+        produto1.setDescricao("Sem glúten e sem leite");
+
+        produto2.setId(2L);
+        produto2.setNome("Empanadas");
+        produto2.setPreco(5.0);
+        produto2.setDescricao("Diversos sabores");
+
+        produto3.setId(3L);
+        produto3.setNome("Docinhos");
+        produto3.setPreco(2.0);
+        produto3.setDescricao("Brigadeiro, beijinho e leite ninho");
+
+        produtoList.add(produto1);
+        produtoList.add(produto2);
+        produtoList.add(produto3);
+
+        return ResponseEntity.ok(produtoList);
     }
 }
