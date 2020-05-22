05da6efda008b1abff5e816b3b3f4471679e45b7
Catherine Korres <cathy.korres@gmail.com>
2020-05-21 22:32:01 -0300

    Adicionando endpoints de Product

--- src/main/java/com/github/classificadosamac/api/controller/ProductController.java
+++ src/main/java/com/github/classificadosamac/api/controller/ProductController.java
@@ -1,11 +1,10 @@
 package com.github.classificadosamac.api.controller;
 
 import com.github.classificadosamac.api.model.Product;
+import com.github.classificadosamac.api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,34 +12,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> productSave(@RequestBody Product product) {
+        return ResponseEntity.ok(productService.save(product));
+    }
 
     @CrossOrigin(origins = "*")
     @GetMapping
-    public ResponseEntity<List<Product>> getProducts() {
-        List<Product> productList = new ArrayList<>();
-        Product product1 = new Product();
-        Product product2 = new Product();
-        Product product3 = new Product();
-
-        product1.setId(1L);
-        product1.setName("Bolo de maçã");
-        product1.setPrice(30.0);
-        product1.setDescription("Sem glúten e sem leite");
-
-        product2.setId(2L);
-        product2.setName("Empanadas");
-        product2.setPrice(5.0);
-        product2.setDescription("Diversos sabores");
-
-        product3.setId(3L);
-        product3.setName("Docinhos");
-        product3.setPrice(2.0);
-        product3.setDescription("Brigadeiro, beijinho e leite ninho");
-
-        productList.add(product1);
-        productList.add(product2);
-        productList.add(product3);
-
-        return ResponseEntity.ok(productList);
+    public ResponseEntity<List<Product>> list() {
+        return ResponseEntity.ok(productService.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.findOne(id));
+    }
+
 }
