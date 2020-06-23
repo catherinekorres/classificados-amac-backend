05622c9086cfb401d5a3f0180ca1e77f97cc175c
Catherine <catherine@catherinekorres.dev>
2020-06-22 22:27:36 -0300

    Adicionando SwaggerConfig

---
+++ src/main/java/com/github/classificadosamac/api/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.github.classificadosamac.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title ("API Classificados AMAC")
+                .description ("Repositório público: https://github.com/catherinekorres/classificados-amac-backend")
+                .version("1.0.0")
+                .build();
+
+        return apiInfo;
+    }
+}
